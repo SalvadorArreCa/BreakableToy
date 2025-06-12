3765585243aa0f430f8d084232723e5f3fc0f3b6
SalvadorArreCa <salvador.arredondo@encora.com>
2025-06-11 22:24:01 -0600

    Add, Edit, Delete and out-of-stock added, fixed general problems

--- Backend/store/src/main/java/com/dream/store/controller/ItemManager.java
+++ Backend/store/src/main/java/com/dream/store/controller/ItemManager.java
@@ -4,26 +4,145 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.core.type.TypeReference;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.io.File;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;;
 
 @RestController
 public class ItemManager {
     
     @GetMapping(path = "/products")
     @CrossOrigin(origins = "http://localhost:8080")
-    public List<Items> Catalogo() {
+    public List<Items> Catalogue() {
         ObjectMapper mapper = new ObjectMapper();
         try {
             return mapper.readValue(new File("src/main/resources/catalogo.json"), new TypeReference<List<Items>>() {});
         } catch (Exception ina) {
             ina.printStackTrace();
             return null;
+        }
+    }
+
+    @PostMapping(path = "/products")
+    @CrossOrigin(origins = "http://localhost:8080")
+    public ResponseEntity<Map<String, String>> addProduct(@RequestBody Items product){
+        List<Items> Catalogue = Catalogue();
+        Map<String, String> response = new HashMap<>();
+        try {
+            if(product != null){
+                Catalogue.add(product);
+                writeCatalogue(Catalogue);
+                response.put("message", "Producto Agregado Correctamente >w<");
+                return ResponseEntity.ok(response);
+            }
+            else {
+                response.put("message","El producto llego vacio");
+                return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+            }
+        } catch (Exception ina) {
+            ina.printStackTrace();
+            response.put("message","Error al agregar el producto");
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        }  
+    }
+
+    @GetMapping(path = "/categories")
+    @CrossOrigin(origins = "http://localhost:8080")
+    public List<String> Categories(){
+        List<Items> Catalogue = Catalogue();
+        List<String> Categories = new ArrayList<>();
+
+        for(Items item : Catalogue){
+            if(!Categories.contains(item.getCategory())){
+                Categories.add(item.getCategory());
+            }
+        }
+        return Categories;
+    }
+
+    public void writeCatalogue(List<Items> Catalogue){
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            mapper.writeValue(new File("src/main/resources/catalogo.json"), Catalogue);
+        } catch (Exception ina) {
+            ina.printStackTrace();
+        }
+    }
+
+    @DeleteMapping(path = "/delete/{id}")
+    @CrossOrigin(origins = "http://localhost:8080")
+    public ResponseEntity<Map<String, String>> deleteProduct(@PathVariable int id){
+        List<Items> catalogue = Catalogue();
+        Map<String, String> response = new HashMap<>();
+        
+        try {
+            catalogue.removeIf(item -> item.getId() == id);
+            writeCatalogue(catalogue);
+            response.put("message", "El Producto se elimino correctamente");
+            return ResponseEntity.ok(response);
+        } catch (Exception ina) {
+            ina.printStackTrace();
+            response.put("message", "El producto no se pudo eliminar");
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping(path = "/products/{id}")
+    @CrossOrigin(origins = "http://localhost:8080")
+    public ResponseEntity<Map<String, String>> editProduct(@RequestBody Items editedItem, @PathVariable int id){
+        List<Items> catalogue = Catalogue();
+        Map<String, String> response = new HashMap<>();
+        try {
+            for(Items item : catalogue){
+                if(item.getId() == editedItem.getId()){
+                    item.setCategory(editedItem.getCategory());
+                    item.setName(editedItem.getName());
+                    item.setPrice(editedItem.getPrice());
+                    item.setStock(editedItem.getStock());
+                    item.setUpdateDate(editedItem.getUpdateDate());
+                }
+            }
+            writeCatalogue(catalogue);
+            response.put("message", "El producto se edito correctamente >w<");
+            return ResponseEntity.ok(response);
+        } catch (Exception ina) {
+            ina.printStackTrace();
+            response.put("message", "No se pudo editar el producto");
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping(path = "/products/{id}/out-of-stock")
+    @CrossOrigin(origins = "http://localhost:8080")
+    public ResponseEntity<Map<String,String>> outOfStock(@PathVariable int id){
+        List<Items> catalogue = Catalogue();
+        Map<String, String> response = new HashMap<>();
+
+        try {
+            for(Items item : catalogue){
+                if(item.getId() == id) {
+                    item.setStock(0);
+                }
+            }
+            writeCatalogue(catalogue);
+            response.put("message","El Stock se termino para el producto");
+            return ResponseEntity.ok(response);
+        } catch (Exception ina) {
+            ina.printStackTrace();
+            response.put("message","No se pudo editar el producto");
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
         }
     }
 
--- Frontend/dream-store/src/components/Catalogue.tsx
+++ Frontend/dream-store/src/components/Catalogue.tsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect }from "react"
 import Table from 'react-bootstrap/Table'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
 
 interface Items {
     id: number;
@@ -16,42 +17,175 @@ interface Items {
 
 const Catalogue: React.FC = () => {
     const [items, setItems] = useState<Items[]>([]);
+    const [addModal, setAddModal] = useState(false);
+    const [modalMode, setModalMode] = useState(false);
+    const [newCategory, setNewCategory] = useState(false);
+    const [categories, setCategories] = useState<String[]>([]);
+    const defaultValue = {
+        id: 0,
+        category: "",
+        name: "",
+        price: 0.0,
+        stock: 0,
+        expirationDate: "",
+        creationDate: "",
+        updateDate: ""
+    }
+    const [productData, setProductData] = useState<Items>(defaultValue);
+    
 
     useEffect(() => {
         const getCatalogue = async () => {
             try {
-                const response = await fetch("http://localhost:9090/products");
-                if(!response.ok){
-                    throw new Error(`We couldn't fetch the catalogue TTATT: ${response.status}`);
-                }
-                console.table(response);
-                const data = await response.json();
-                setItems(data);
+                const catalogue = await fetch("http://localhost:9090/products");
+                const categories = await fetch("http://localhost:9090/categories");
+                
+                if(!catalogue.ok) throw new Error(`We couldn't fetch the catalogue TTATT: ${catalogue.status}`); 
+                if(!categories.ok) throw new Error(`Error fetching the categories: ${categories.status}`);
+
+                const catalogueData = await catalogue.json();
+                const categoriesData = await categories.json();
+
+                setItems(catalogueData);
+                setCategories(categoriesData);
             } catch (error) {
                 console.error("The data wasn't fetched: ", error);
                 return [];
             }
         };
-
         getCatalogue();
-    },[]);
+    },[items,categories]);
+
+    const addProduct = async () => {
+        if(productData.id === 0){
+            productData.id = (items[items.length - 1].id + 1);
+        }
+
+        productData.creationDate = getCurrentDate();
+        productData.updateDate = getCurrentDate();
+
+        console.table(productData);
+
+        try {
+            const response = await fetch("http://localhost:9090/products", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(productData)
+            });
+
+            if(!response.ok){
+                throw new Error(`Error: ${response.status}`);
+            }
+
+            setAddModal(false);
+            setNewCategory(false);
+
+            const data = await response.json();
+            console.log("Server Response: ", data);
+        } catch (error) {
+            console.error("Error: ", error);
+        }
+        setProductData(defaultValue);
+    }
+
+    const getCurrentDate = (): String => {
+        const today = new Date();
+        const year = today.getFullYear();
+        const month = String(today.getMonth() + 1).padStart(2,'0');
+        const day = String(today.getDate()).padStart(2,'0');
+
+        return `${year}-${month}-${day}`
+    }
+
+    const editProduct = async () => {
+        productData.updateDate = getCurrentDate();
+
+        try{
+            const response = await fetch(`http://localhost:9090/products/${productData.id}`, {
+                method: "PUT",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(productData)
+            });
+
+            if(!response.ok) throw new Error(`Error while fetching: ${response.status}`);
+
+            setAddModal(false);
+            setNewCategory(false);
+
+            const data = await response.json();
+            console.log("Server Response: ", data);
+        } catch(error) {
+            console.error("Error: ", error);
+        }
+
+        setProductData(defaultValue);
+    }
+
+    const setOutOfStock = async (id: number) => {
+        try {
+            const response = await fetch(`http://localhost:9090/products/${id}/out-of-stock`,{
+                method:"PUT",
+                headers: { "Content-Type": "application/json" },
+            })
+
+            if(!response.ok) throw new Error(`Error while fetching: ${response.status}`);
+
+            const data = await response.json();
+            console.log("Server Response: ", data);
+        } catch (error) {
+            console.error("Error: ", error)
+        }
+        setProductData(defaultValue);
+    }
+
+    const deleteProduct = async (id: number) => {
+        try {
+            const response = await fetch(`http://localhost:9090/delete/${id}`,{
+                method:"DELETE",
+                headers: {"Content-Type": "application/json"},
+            });
+
+            if(!response.ok) throw new Error("Couldn't DELETE the product");
+
+            const data = response.json();
+            console.log("Server Response: ", data)
+        } catch (error) {
+            console.error("Error: ", error);
+        }
+    }
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setProductData({...productData, [name]: value, });
+
+    };
+
+    const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const { name, value } = e.target;
+        setProductData({...productData, [name]: value});
+    }
 
     return(
         <>
         <div className="flex flex-col bg-neutral-700 h-full px-50 py-10">
             <p className="text-6xl font-bold text-slate-200 text-center">Catalogo</p>
+            
+            {/* FILTROS DE BUSQUEDA */}
+            
             <div className="flex flex-row bg-neutral-500 rounded-xl">
                 <div className="flex flex-col p-4 basis-1/2">
                     <Form.Group className="flex flex-row items-center justify-center p-2">
                         <Form.Label className="mx-3">Name</Form.Label>
-                        <Form.Control type="text" id="input-name "/>
+                        <Form.Control type="text" id="input-name-search"/>
                     </Form.Group>
                     <Form.Group className="flex flex-row items-center p-2">
                         <Form.Label className="mx-3">Category</Form.Label>
                         <Form.Select aria-label="Default select example">
                             <option>Select a category</option>
-                            {items.map((item) => (
-                                <option value={String(item.category)}>{item.category}</option>
+                            {categories.map((category) => (
+                                <option key={String(category)} value={String(category)}>{category}</option>
                             ))}
                         </Form.Select>
                     </Form.Group>
@@ -63,38 +197,152 @@ const Catalogue: React.FC = () => {
                             <option value="2">Out of Stock</option>
                             <option value="3">All</option>
                         </Form.Select>
-                        <Button className="mx-4" variant="primary"> Search</Button>
+                        <Button type="submit" className="mx-4" variant="primary">Search</Button>
                     </Form.Group>
                 </div>
                 <div className="bg-red-300 basis-1/2"></div>
             </div>
             <div className="content-start">
-                <Button className="m-4" variant="primary">Agregar Producto</Button>
+                <Button className="m-4" variant="primary" onClick={() => {
+                    setProductData(defaultValue);
+                    setAddModal(true);
+                    setModalMode(true);
+                }}>Agregar Producto</Button>
+                
+                {/* AGREGAR PRODUCTO */}
+
+                <Modal
+                    size="lg"
+                    show={addModal}
+                    onHide={() => {setAddModal(false)}}
+                    aria-labelledby="add-product"
+                >
+                    <Modal.Header closeButton className="bg-neutral-900">
+                            <Modal.Title id="add-product">Add Product</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body className="bg-neutral-800">
+                        <Form.Group>
+                            <Form.Label className="text-slate-200">Name</Form.Label>
+                            <Form.Control 
+                                type="text" 
+                                id="input-name-add" 
+                                name="name"
+                                value={String(productData.name)}
+                                onChange={handleChange}
+                            ></Form.Control>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label className="text-slate-200">Category</Form.Label>
+                            {newCategory ? (
+                                <Form.Control 
+                                    type="text"
+                                    id="input-category-add"
+                                    name="category"
+                                    value={String(productData.category)}
+                                    onChange={handleChange}
+                                ></Form.Control>
+                            ) : (
+                                <Form.Select aria-label="Default select example" name="category" onChange={handleSelectChange}>
+                                    <option>Select a category</option>
+                                    {categories.map((category) => (
+                                        <option key={String(category)} value={String(category)}>{category}</option>
+                                    ))}
+                                </Form.Select>
+                            )}
+                            <Button variant="outline-secondary" onClick={() => {
+                                if(!newCategory) setNewCategory(true);
+                                else setNewCategory(false);
+                            }}> + </Button>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label className="text-slate-200">Price</Form.Label>
+                            <Form.Control 
+                                type="text"
+                                id="input-price-add"
+                                name="price"
+                                value={productData.price}
+                                onChange={handleChange}
+                            ></Form.Control>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label className="text-slate-200">Stock</Form.Label>
+                            <Form.Control 
+                                type="text"
+                                id="input-stock-add"
+                                name="stock"
+                                value={Number(productData.stock)}
+                                onChange={handleChange}
+                            ></Form.Control>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label className="text-slate-200">Expiration Date</Form.Label>
+                            <Form.Control 
+                                type="text"
+                                id="input-exDate-add"   
+                                name="expirationDate"
+                                value={String(productData.expirationDate)}
+                                onChange={handleChange}
+                            ></Form.Control>
+                        </Form.Group>
+                        <div>
+                            {modalMode ? (
+                                <Button type="submit" variant="primary" onClick={() => {addProduct()}}>Add</Button>
+                            ) : (
+                                <Button type="submit" variant="primary" onClick={() => {editProduct()}}>Edit</Button>
+                            )}
+                            <Button variant="danger" onClick={() => setAddModal(false)}>Cancel</Button>
+                        </div>
+                    </Modal.Body>
+                </Modal>
             </div>
+
+            {/* TABLA DE PRODUCTOS */}
+
             <Table striped bordered hover variant="dark">
                 <thead>
                     <tr>
+                        <th></th>
                         <th>Id</th>
                         <th>Categoria</th>
                         <th>Nombre</th>
                         <th>Precio</th>
                         <th>Stock</th>
                         <th>Fecha de Caducidad</th>
+                        <th></th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
                     {items.map((item) => (
-                        <tr>
+                        <tr key={item.id}>
+                            <td className="text-center">
+                                <Button variant="outline-secondary" size="sm" onClick={() => setOutOfStock(item?.id)}> X </Button>
+                            </td>
                             <td>{item?.id}</td>
                             <td>{item?.category}</td>
                             <td>{item?.name}</td>
                             <td>{item?.price}</td>
                             <td>{item?.stock}</td>
                             <td>{item?.expirationDate}</td>
+                            <td className="text-center">
+                                <Button variant="primary" size="sm" onClick={() => {
+                                    setProductData(item);
+                                    setAddModal(true);
+                                    setModalMode(false);
+                                }}>Edit</Button>
+                            </td>
+                            <td className="text-center">
+                                <Button variant="danger" size="sm" onClick={() => deleteProduct(item.id)}>Delete</Button>
+                            </td>
                         </tr>
                     ))}
                 </tbody>
             </Table>
+            <div className="text-center">
+                <Button variant="dark">1</Button>
+            </div>
+
+            {/* Metricas */}
         </div>
         </>
     );
